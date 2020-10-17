384b79298db1f8693621088eb937d48afdb0edac
Binod Shrestha <shresthabinod.softdev@gmail.com>
2020-10-17 18:50:25 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/* Example:01
+function Hello({library, message, number}){
+    return(
+        <div>
+            <h1>Welcome to {library}</h1>
+            <p>{message}</p>
+            <p>{number} Props total</p>
+        </div>
+    )
+
+}*/
+const lakeList = [
+    "Lake Huron",
+    "Lake Ontario",
+    "Professor's Lake"
+]
+/*function Lake(name){
+    return(
+        <h1>{name}</h1>
+    )
+}*/
+function App(lakes){
+    return (
+       /* <div>
+            <Lake name="Lake Huron" />
+            <Lake name="Lake Ontario" />
+            <Lake name="Professor Lake" />
+        </div>*/
+        <ul>
+            {lakes.map(lake =>(<li>{lake}</li>))}
+        </ul>
+    );
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  /*Example:01
+  <Hello
+      library= "React"
+      message="add dynamic data"
+      number={3}
+      />,*/
+    <App lakes={lakeList}/>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
